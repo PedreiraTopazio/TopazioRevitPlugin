d0298a5cc083671995781975984a09596ecb85c9
RickBIM <hr.furlaneto@gmail.com>
2024-01-31 10:08:24 -0300

    Add project files.

---
+++ TopazioRevitPluginShared/MatchOverrides.cs
@@ -0,0 +1,67 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using TopazioRevitPlugin2022;
+
+namespace TopazioRevitPluginShared
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    internal class MatchOverrides : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and document objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+
+            try
+            {
+                //Define a reference Object to accept the pick result
+                Reference pickedref = null;
+
+                //Pick a element
+                Selection sel = uiapp.ActiveUIDocument.Selection;
+                AllFilter filter = new AllFilter();
+                pickedref = sel.PickObject(ObjectType.Element, filter, "Select source element");
+                Element elem = doc.GetElement(pickedref);
+
+                OverrideGraphicSettings currentOverride = doc.ActiveView.GetElementOverrides(elem.Id);
+
+
+                //Pick a element
+                sel = uiapp.ActiveUIDocument.Selection;
+                //Define a reference Object to accept the pick result
+                IList<Reference> pickedrefs = null;
+                pickedrefs = sel.PickObjects(ObjectType.Element, filter, "Select destination elements");
+
+                Transaction trans = new Transaction(doc);
+                trans.Start("Override Graphics");
+                foreach(Reference elem_reference in pickedrefs)
+                {
+                    elem = doc.GetElement(elem_reference);
+                    doc.ActiveView.SetElementOverrides(elem.Id, currentOverride);
+                }
+                trans.Commit();
+
+            }
+            //If the user right-clicks or presses Esc, handle the exception
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            } catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ TopazioRevitPluginShared/TopazioPanel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Controls;
+
+namespace TopazioRevitPluginShared
+{
+    class TopazioPanel : IExternalApplication
+    {
+        
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string topazioTab = "Topazio";
+            List<string> topazioPanels = new List<string>();
+            topazioPanels.Add("Visualização");
+            
+            //Creating Tab
+            application.CreateRibbonTab(topazioTab);
+            
+
+            //Creating Panels
+            foreach(string panel in topazioPanels)
+            {
+                application.CreateRibbonPanel(topazioTab, panel);
+            }
+
+            //Creating buttons
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            RibbonPanel panel_Visualizacao = RibbonPanel(application, "Topazio", "Visualização");
+            if(panel_Visualizacao.AddItem(new PushButtonData("Match Overrides", "Match Overrides", thisAssemblyPath, "TopazioRevitPluginShared.MatchOverrides")) is PushButton button)
+            {
+                button.ToolTip = "Esse comando iguala as sobreposições de elementos na vista";
+                // Reflection of path to image 
+                var globePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "MatchOverrideGraphics.png"); //Mudar path depois para poder pegar outras imagens
+                Uri uriImage = new Uri(globePath);
+                // Apply image to bitmap
+                BitmapImage largeImage = new BitmapImage(uriImage);
+                // Apply image to button 
+                button.LargeImage = largeImage;
+            }
+
+            return Result.Succeeded;
+        }
+
+        public RibbonPanel RibbonPanel(UIControlledApplication application, string tab, string panelName)
+        {
+
+            RibbonPanel ribbonPanel = null;
+            List<RibbonPanel> panels = application.GetRibbonPanels(tab);
+            foreach (RibbonPanel panel in panels.Where(panel => panel.Name == panelName))
+            {
+                ribbonPanel = panel;
+            }
+
+            return ribbonPanel;
+        }
+    }
+}
